0c2ad1e7182effed65631d5ad2e72a95c02de065
99003753 <78870150+99003753@users.noreply.github.com>
2021-03-12 10:21:36 +0530

    Add files via upload

---
+++ Basic_Calculation/Basic calculation.py
@@ -0,0 +1,26 @@
+
+print("----------Arithmetic Calculator---------- ")   
+print("for Addition Press'+'")
+print("for Subtraction Press'-'")
+print("for Multiplication Press'*'")
+print("for Division Press'/'")
+      
+    
+while True:
+        choice = input("Enter Your Choice:")
+        a = int(input("Enter first number: "))
+        b = int(input("Enter first number: "))
+        
+        if choice =='+':
+            print("Sum is",a+b)
+        elif choice == '-':
+            print("Subtraction is",a-b)
+        elif choice =='*':
+            print("Multiplication is",a*b)
+        elif choice =='/':
+            print("Division is",a/b)
+            break
+        else:
+            print("Invalid input")
+
+        
