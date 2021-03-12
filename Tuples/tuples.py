4b0a88c07a296532146e6c4a76f4e793400301a8
99003753 <78870150+99003753@users.noreply.github.com>
2021-03-12 18:29:34 +0530

    from Linux VS_CODE

---
+++ Tuples/tuples.py
@@ -0,0 +1,26 @@
+a = (1,2,3,"Ashu",22.400)
+b = 1,2,3,4,5,6
+# printing the tuples
+print(type(a))
+print(a)
+print(b)
+
+
+
+# single alphabet covert in word
+tup=('W','E','L','C','O','M','E','t','o','L','T','T','S')
+str= ''.join(tup)
+print(str)
+
+
+# Removing individual tuple elements is not possible.
+
+
+# length of tuples
+print(len(a))
+print(len(b))
+
+# we find maximum element of tuple b
+print(max(b))
+# we find minimum element of tuple b
+print(min(b))
